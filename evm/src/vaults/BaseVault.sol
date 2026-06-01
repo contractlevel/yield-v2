e21e318584f1c014ceba25c5c783a45436272e66
contract-level <contractlevel@proton.me>
2026-06-01 14:47:21 +0100

    review comment

--- evm/src/vaults/BaseVault.sol
+++ evm/src/vaults/BaseVault.sol
@@ -452,8 +452,9 @@ abstract contract BaseVault is Pausable, AccessControlDefaultAdminRules, Reentra
     /// @dev Precondition: amount must be more than 0
     /// @dev Precondition: the strategy deposit operation must succeed
     /// @dev Precondition: the call must not be reentered
-    // @review should this be kyc gated? argument for: consistent with protocol, regulators know who donated
+    // @review-legal should this be kyc gated? argument for: consistent with protocol, regulators know who donated
     // argument against: generated yield from strategy protocol borrowers are not kyc'd, and it puts significant limitation on this feature
+    // only ParentVault currently has ACE PolicyProtection
     function donate(uint256 amount) external nonReentrant {
         if (amount == 0) revert BaseVault__NoZeroAmount();
 
