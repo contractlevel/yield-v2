96d9de66161f5f329c2477b474e31602e7ce2a1d
contract-level <contractlevel@proton.me>
2026-06-13 15:29:28 +0100

    fix natspec

--- evm/certora/harness/HelperHarness.sol
+++ evm/certora/harness/HelperHarness.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.28;
 
 /// @title HelperHarness
 /// @author @contractlevel
-/// @notice Helper functions to use 
+/// @notice HelperHarness to use in CVL specs
 contract HelperHarness {
     function bytes32ToAddress(bytes32 b) external returns (address) {
         return address(uint160(uint256(b)));
